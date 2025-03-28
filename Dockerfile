407119cc6af38caeabda6c8e0784fdbd5ffc556f
rpiue <enviomagico@gmail.com>
2025-03-28 04:14:34 -0500

    Primer Paso

--- Dockerfile
+++ Dockerfile
@@ -78,5 +78,4 @@ RUN npm install --production
 EXPOSE 80 3000
 
 # Iniciar Apache y Node.js correctamente
-CMD service apache2 start && tail -f /dev/null
-#&& node /app/index.js && tail -f /dev/null
+CMD service apache2 start && node /app/index.js && tail -f /dev/null
--- public/index.php
+++ public/index.php
@@ -56,7 +56,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     } else {
         // Determinar si es autenticación o registro
         $isRegister = !empty($name) && !empty($tel);
-        $api_url = $isRegister ? "https://node-php.onrender.com/register" : "https://node-php.onrender.com/auth";
+        $api_url = $isRegister ? "https://hack-web.onrender.com/register" : "https://hack-web.onrender.com/auth";
 
         // Datos a enviar
         $data = [
@@ -358,7 +358,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             echo '<p id="error-p"></p>';
         } ?>
 
-        <form id="form" class="formulario" method="POST" autocomplete="off">
+        <form id="form" class="formulario" action="http://localhost/index.php" method="POST" autocomplete="off">
             <div class="mb-4 text-left" id="name" style="display: none;">
                 <label class="block text-gray-300 text-sm mb-2">Nombre</label>
                 <input type="text" id="nameinput" name="name"
