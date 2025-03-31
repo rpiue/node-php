870008050dc82eed1a22690db5b18c80796b2c80
rpiue <enviomagico@gmail.com>
2025-03-31 00:05:24 -0500

    Primer Paso

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const server = http.createServer(app);
 const io = socketIo(server); // Inicializamos Socket.IO
 // Middleware para servir archivos estáticos desde la carpeta "public"
 // Redirige "/dashboard" a "dashboard.php" y permite que Apache lo procese
+app.use(express.static("public"));
 
 app.use((req, res, next) => {
   console.log(`🛠️ Nueva petición: ${req.method} ${req.url}`);
@@ -33,7 +34,6 @@ app.use((req, res, next) => {
 });
 
 
-app.use(express.static("public"));
 
 app.use(
   "/login",
